6760506d5cd27b1ae75b0cbdb6064898eaef57fb
Nemo1166 <zhangguoxi1166@gmail.com>
2024-05-13 08:36:53 +0100

    mod sitemeta

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,9 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+import { getDirname, path } from 'vuepress/utils'
+
+const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
   base: "/",
@@ -9,6 +13,13 @@ export default defineUserConfig({
   description: "泰拉通讯枢纽存档",
 
   theme,
+
+  plugins: [
+    registerComponentsPlugin({
+      // 配置项
+      componentsDir: path.resolve(__dirname, './components'),
+    }),
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -87,7 +87,7 @@ export default hopeTheme({
   // 在这里配置主题提供的插件
   plugins: {
     blog: true,
-
+    seo: false,
     // 启用之前需安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
     // comment: {
