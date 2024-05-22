1227fe38e380f975a81a6a47e2a444d73e1a7fbb
Nemo1166 <zhangguoxi1166@gmail.com>
2024-05-22 23:22:50 +0100

    fix disable dev page

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -2,7 +2,7 @@ import { defineClientConfig } from 'vuepress/client'
 import summary from './layouts/summary.vue'
 
 export default defineClientConfig({
-  layouts: {
-    summary,
-  },
+  // layouts: {
+  //   summary,
+  // },
 })
